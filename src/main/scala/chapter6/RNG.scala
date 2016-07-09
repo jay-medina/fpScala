0b494cf526916ab72154bd38d67ebc39ff7293bc
Jose Medina <josemedina921@gmail.com>
2016-07-09 13:18:11 -0700

    Solutions to exercises

--- src/main/scala/chapter6/RNG.scala
+++ src/main/scala/chapter6/RNG.scala
@@ -15,11 +15,74 @@ case class SimpleRNG(seed: Long) extends RNG {
 }
 
 object RNG2 {
+
+  def randomPair(rng: RNG): ((Int,Int), RNG) = {
+    val (i1, rng2) = rng.nextInt
+    val (i2, rng3) = rng2.nextInt
+
+    ((i1,i2), rng3)
+  }
+
+  /* exercise 6.1 */
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val (i ,r) = rng.nextInt
+
+    (if (i < 0) (-i + 1) else i, r)
+  }
+
+  /* exercise 6.2 */
+  def double(rng: RNG): (Double, RNG) = {
+    val (i, r) = nonNegativeInt(rng)
+
+    (i / (Int.MaxValue.toDouble + 1), r)
+  }
+
+  /* exercise 6.3 */
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (i, rng2) = rng.nextInt
+    val (d, rng3) = double(rng2)
+
+    ((i, d), rng3)
+  }
+
+  /* exercise 6.3 */
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val ((i1, d1), rng2) = intDouble(rng)
+
+    ((d1, i1), rng2)
+  }
+
+  /* exercise 6.3 */
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (d1, r1) = double(rng)
+    val (d2, r2) = double(r1)
+    val (d3, r3) = double(r2)
+
+    ((d1,d2,d3), r3)
+  }
+
+  /* exercise 6.4 */
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    def loop(ct: Int, rng: RNG, acc: List[Int]): (List[Int], RNG) = {
+      if(ct <= 0) (acc, rng)
+      else {
+        val (i1, r1) = rng.nextInt
+        loop(ct - 1, r1, i1 :: acc)
+      }
+    }
+
+    loop(count, rng, Nil)
+  }
+
   def main(args: Array[String]) = {
 
     val myRNG = SimpleRNG(42)
     val (n1, rng2) = myRNG.nextInt
 
-    println(n1, rng2)
+    //println(n1, rng2)
+
+    //println(double(rng2))
+
+    (ints(4)(myRNG))._1.foreach(println)
   }
 }
